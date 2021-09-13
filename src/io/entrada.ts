6d0abb96ab2ee02a35256011673a625d0949a757
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2021-09-13 17:45:14 -0300

    Inclusão da classe de entrada de dados

---
+++ src/io/entrada.ts
@@ -0,0 +1,14 @@
+import promptSync from 'prompt-sync';
+export default class Entrada {
+    public receberNumero(mensagem: string): number {
+        let prompt = promptSync();
+        let valor = prompt(mensagem)
+        let numero  = new Number(valor)
+        return numero.valueOf()
+    }
+    public receberTexto(mensagem: string): string {
+        let prompt = promptSync();
+        let texto = prompt(mensagem)
+        return texto
+    }
+}
